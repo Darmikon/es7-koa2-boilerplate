33ccd4cf670b0d9bab701d9e1f542cab7399308d
Darmikon <hellfrom@mail.ru>
2017-07-24 06:49:53 +0300

    sql fix

--- src/module/db/create_user.sql
+++ src/module/db/create_user.sql
@@ -1,7 +1,23 @@
 -- should be executed before connection via terminal for example
 -- SELECT * FROM pg_roles;
-DROP ROLE IF EXISTS username;
-CREATE USER username WITH PASSWORD 'passwd';
-REVOKE ALL ON ALL TABLES IN SCHEMA scheme_name FROM PUBLIC;
-GRANT USAGE ON SCHEMA scheme_name TO username;
-GRANT SELECT ON ALL TABLES IN SCHEMA scheme_name TO username;
+-- This create user if not exists
+do
+$body$
+declare
+  num_users integer;
+begin
+   SELECT count(*)
+     into num_users
+   FROM pg_user
+   WHERE usename = 'darmikon';
+
+   IF num_users = 0 THEN
+      CREATE ROLE darmikon LOGIN PASSWORD 'pass';
+--    CREATE USER darmikon WITH PASSWORD 'pass';
+   END IF;
+end
+$body$
+;
+REVOKE ALL ON ALL TABLES IN SCHEMA schema_name FROM PUBLIC;
+GRANT USAGE ON SCHEMA schema_name TO darmikon;
+GRANT SELECT ON ALL TABLES IN SCHEMA schema_name TO darmikon;
